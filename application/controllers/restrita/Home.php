9dd93afcc85e76bce6194be7cdb31e547be4e8a5
carloscajado <cajadomotos@gmail.com>
2021-11-15 16:21:46 -0300

    quase100

--- application/controllers/restrita/Home.php
+++ application/controllers/restrita/Home.php
@@ -7,10 +7,12 @@ class Home extends CI_Controller{
     public function __construct(){
         parent::__construct();
 
-        //Existe uma sessã:o
+        //Existe uma sessão
     }
     public function index(){
 
+        $this->load->view('restrita/layout/header');
         $this->load->view('restrita/home/index');
+        $this->load->view('restrita/layout/footer');
     }
 }
---
+++ application/views/restrita/layout/footer.php
@@ -0,0 +1,23 @@
+
+ <footer class="main-footer">
+            <div class="footer-left">
+            <a href="templateshub.net">Templateshub</a></a>
+            </div>
+            <div class="footer-right">
+            </div>
+        </footer>
+        </div>
+   </div>
+        <!-- General JS Scripts -->
+        <script src="assets/js/app.min.js"></script>
+        <!-- JS Libraies -->
+        <!-- Page Specific JS File -->
+        <!-- Template JS File -->
+        <script src="assets/js/scripts.js"></script>
+        <!-- Custom JS File -->
+        <script src="assets/js/custom.js"></script>
+    </body>
+
+
+<!-- blank.html  21 Nov 2019 03:54:41 GMT -->
+</html>
