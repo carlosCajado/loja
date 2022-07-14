3018c9ef703b5fa3affde6542a6a00b0eb2d7669
carlos <cajadomotos@gmail.com>
2022-07-14 17:45:46 -0300

    commit web/produto.php

--- application/views/web/produto.php
+++ application/views/web/produto.php
@@ -44,7 +44,13 @@
                 <div class="product-details-view-content pt-60">
                     <div class="product-info">
                         <h2><?php echo $produto->produto_nome?></h2>
-                        <span class="product-details-ref">Reference: demo_15</span>
+                        <span class="product-details-ref">Código: <?php echo $produto->produto_codigo?></span>
+                        <p>
+                        <span class="product-details-ref">Estoque: <?php echo ($produto->produto_quantidade_estoque >0 ? '<span class"badge badge-success">' . $produto->produto_quantidade_estoque.'</span>'  : '<span class"badge badge-danger">Indisponivel</span>')?></span>
+                        </p>
+                        <p>
+                        <span class="product-details-ref"><a href="<?php echo base_url('marca/'.$produto->marca_meta_link); ?>"><?php echo $produto->marca_nome?></a></span>
+                        </p>
                         <div class="rating-box pt-20">
                             <ul class="rating rating-with-review-item">
                                 <li><i class="fa fa-star-o"></i></li>
@@ -58,74 +64,37 @@
                         </div>
                         <div class="price-box pt-20">
                             <span class="new-price new-price-2"><?php echo 'R$&nbsp'.$produto->produto_valor?></span>
+                            <a class="wishlist-btn" href="wishlist.html"><i class="fa fa-heart-o"></i> favorito</a>
                         </div>
                         <div class="product-desc">
                             <p>
-                                <span> <?php echo $produto->produto_descricao?>
-                                </span>
                             </p>
                         </div>
                         <div class="product-variants">
-                            <div class="produt-variants-size">
+                            <!-- <div class="produt-variants-size">
                                 <label>Dimension</label>
                                 <select class="nice-select">
                                     <option value="1" title="S" selected="selected"><?php echo $produto->produto_tamanho?></option>
                                     <option value="2" title="M">60x90cm</option>
                                     <option value="3" title="L">80x120cm</option>
                                 </select>
-                            </div>
+                            </div> -->
                         </div>
                         <div class="single-add-to-cart">
                             <form action="#" class="cart-quantity">
                                 <div class="quantity">
-                                    <label>Quantity</label>
+                                    <label>Quantidade</label>
                                     <div class="cart-plus-minus">
                                         <input class="cart-plus-minus-box" value="1" type="text">
                                         <div class="dec qtybutton"><i class="fa fa-angle-down"></i></div>
                                         <div class="inc qtybutton"><i class="fa fa-angle-up"></i></div>
                                     </div>
                                 </div>
-                                <button class="add-to-cart" type="submit">Add to cart</button>
+                                <button class="add-to-cart" type="submit">Carrinho</button>
                             </form>
                         </div>
                         <div class="product-additional-info pt-25">
-                            <a class="wishlist-btn" href="wishlist.html"><i class="fa fa-heart-o"></i>Add to wishlist</a>
-                            <div class="product-social-sharing pt-25">
-                                <ul>
-                                    <li class="facebook"><a href="#"><i class="fa fa-facebook"></i>Facebook</a></li>
-                                    <li class="twitter"><a href="#"><i class="fa fa-twitter"></i>Twitter</a></li>
-                                    <li class="google-plus"><a href="#"><i class="fa fa-google-plus"></i>Google +</a></li>
-                                    <li class="instagram"><a href="#"><i class="fa fa-instagram"></i>Instagram</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="block-reassurance">
-                            <ul>
-                                <li>
-                                    <div class="reassurance-item">
-                                        <div class="reassurance-icon">
-                                            <i class="fa fa-check-square-o"></i>
-                                        </div>
-                                        <p>Security policy (edit with Customer reassurance module)</p>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="reassurance-item">
-                                        <div class="reassurance-icon">
-                                            <i class="fa fa-truck"></i>
-                                        </div>
-                                        <p>Delivery policy (edit with Customer reassurance module)</p>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="reassurance-item">
-                                        <div class="reassurance-icon">
-                                            <i class="fa fa-exchange"></i>
-                                        </div>
-                                        <p> Return policy (edit with Customer reassurance module)</p>
-                                    </div>
-                                </li>
-                            </ul>
+
                         </div>
                     </div>
                 </div>
@@ -141,9 +110,7 @@
             <div class="col-lg-12">
                 <div class="li-product-tab">
                     <ul class="nav li-product-menu">
-                        <li><a class="active" data-toggle="tab" href="#description"><span>Description</span></a></li>
-                        <li><a data-toggle="tab" href="#product-details"><span>Product Details</span></a></li>
-                        <li><a data-toggle="tab" href="#reviews"><span>Reviews</span></a></li>
+                        <li><a class="active" data-toggle="tab" href="#description"><span>Descrição</span></a></li>
                     </ul>
                 </div>
                 <!-- Begin Li's Tab Menu Content Area -->
@@ -152,8 +119,7 @@
         <div class="tab-content">
             <div id="description" class="tab-pane active show" role="tabpanel">
                 <div class="product-description">
-                    <span>The best is yet to come! Give your walls a voice with a framed poster. This aesthethic, optimistic poster will look great in your desk or in an open-space office. Painted wooden frame with passe-partout for more depth.</span>
-                </div>
+                    <span> <?php echo $produto->produto_descricao?></span>
             </div>
             <div id="product-details" class="tab-pane" role="tabpanel">
                 <div class="product-details-manufacturer">
