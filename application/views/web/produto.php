13fc138b5597fcbad1b20eac9845de6587bc4691
carlos <cajadomotos@gmail.com>
2022-08-05 16:11:25 -0300

    adicionado o calculo frete

--- application/views/web/produto.php
+++ application/views/web/produto.php
@@ -50,8 +50,8 @@
                         </p>
                         <p>
                         <span class="product-details-ref"><a href="<?php echo base_url('marca/'.$produto->marca_meta_link); ?>"><?php echo $produto->marca_nome?></a></span>
-                        </p>
-                        <div class="rating-box pt-20">
+                        </p>    
+                        <!-- <div class="rating-box pt-20">
                             <ul class="rating rating-with-review-item">
                                 <li><i class="fa fa-star-o"></i></li>
                                 <li><i class="fa fa-star-o"></i></li>
@@ -61,7 +61,7 @@
                                 <li class="review-item"><a href="#">Read Review</a></li>
                                 <li class="review-item"><a href="#">Write Review</a></li>
                             </ul>
-                        </div>
+                        </div> -->
                         <div class="price-box pt-20">
                             <span class="new-price new-price-2"><?php echo 'R$&nbsp'.$produto->produto_valor?></span>
                             <a class="wishlist-btn" href="wishlist.html"><i class="fa fa-heart-o"></i> favorito</a>
@@ -83,17 +83,33 @@
                         <div class="single-add-to-cart">
                             <form action="#" class="cart-quantity">
                                 <div class="quantity">
-                                    <label>Quantidade</label>
-                                    <div class="cart-plus-minus">
-                                        <input class="cart-plus-minus-box" value="1" type="text">
-                                        <div class="dec qtybutton"><i class="fa fa-angle-down"></i></div>
-                                        <div class="inc qtybutton"><i class="fa fa-angle-up"></i></div>
+                                    <label>Calcular Entrega</label>
+                                    <div class="" style="min-width:140px; float:left; margin-right: 10px; position:relative; text-align:left; ">
+                                        <input  style="background-color:aliceblue" class="cart-plus-minus-box" type="text" placeholder="Informe seu CEP">
                                     </div>
                                 </div>
-                                <button class="add-to-cart" type="submit">Carrinho</button>
+                                <button class="add-to-cart2" type="submit"><img src="<?php echo base_url('public/web/images/entrega.png'); ?>" width="40px" alt=""></button>
+                            </form>
+                        </div>
+
+                        <div class="single-add-to-cart">
+                            <form action="#" class="cart-quantity">
+                                <div class="quantity">
+                                    <!-- <label>Quantidade</label> -->
+                                    <div class="">
+                                        <!-- <input class="cart-plus-minus-box" value="1" type="text">
+                                        <div class="dec qtybutton"><i class="fa fa-angle-down"></i></div>
+                                        <div class="inc qtybutton"><i class="fa fa-angle-up"></i></div> -->
+                                    </div>
+                                </div>
+                                <button class="add-to-cart" type="submit">Adicionar ao Carrinho</button>
                             </form>
                         </div>
                         <div class="product-additional-info pt-25">
+                        <div class="product-desc">
+                            <p>
+                            </p>
+                        </div>
 
                         </div>
                     </div>
@@ -120,112 +136,6 @@
             <div id="description" class="tab-pane active show" role="tabpanel">
                 <div class="product-description">
                     <span> <?php echo $produto->produto_descricao?></span>
-            </div>
-            <div id="product-details" class="tab-pane" role="tabpanel">
-                <div class="product-details-manufacturer">
-                    <a href="#">
-                        <img src="images/product-details/1.jpg" alt="Product Manufacturer Image">
-                    </a>
-                    <p><span>Reference</span> demo_7</p>
-                    <p><span>Reference</span> demo_7</p>
-                </div>
-            </div>
-            <div id="reviews" class="tab-pane" role="tabpanel">
-                <div class="product-reviews">
-                    <div class="product-details-comment-block">
-                        <div class="comment-review">
-                            <span>Grade</span>
-                            <ul class="rating">
-                                <li><i class="fa fa-star-o"></i></li>
-                                <li><i class="fa fa-star-o"></i></li>
-                                <li><i class="fa fa-star-o"></i></li>
-                                <li class="no-star"><i class="fa fa-star-o"></i></li>
-                                <li class="no-star"><i class="fa fa-star-o"></i></li>
-                            </ul>
-                        </div>
-                        <div class="comment-author-infos pt-25">
-                            <span>HTML 5</span>
-                            <em>01-12-18</em>
-                        </div>
-                        <div class="comment-details">
-                            <h4 class="title-block">Demo</h4>
-                            <p>Plaza</p>
-                        </div>
-                        <div class="review-btn">
-                            <a class="review-links" href="#" data-toggle="modal" data-target="#mymodal">Write Your Review!</a>
-                        </div>
-                        <!-- Begin Quick View | Modal Area -->
-                        <div class="modal fade modal-wrapper" id="mymodal">
-                            <div class="modal-dialog modal-dialog-centered" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-body">
-                                        <h3 class="review-page-title">Write Your Review</h3>
-                                        <div class="modal-inner-area row">
-                                            <div class="col-lg-6">
-                                                <div class="li-review-product">
-                                                    <img src="images/product/large-size/3.jpg" alt="Li's Product">
-                                                    <div class="li-review-product-desc">
-                                                        <p class="li-product-name">Today is a good day Framed poster</p>
-                                                        <p>
-                                                            <span>Beach Camera Exclusive Bundle - Includes Two Samsung Radiant 360 R3 Wi-Fi Bluetooth Speakers. Fill The Entire Room With Exquisite Sound via Ring Radiator Technology. Stream And Control R3 Speakers Wirelessly With Your Smartphone. Sophisticated, Modern Design </span>
-                                                        </p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="col-lg-6">
-                                                <div class="li-review-content">
-                                                    <!-- Begin Feedback Area -->
-                                                    <div class="feedback-area">
-                                                        <div class="feedback">
-                                                            <h3 class="feedback-title">Our Feedback</h3>
-                                                            <form action="#">
-                                                                <p class="your-opinion">
-                                                                    <label>Your Rating</label>
-                                                                    <span>
-                                                                        <select class="star-rating">
-                                                                            <option value="1">1</option>
-                                                                            <option value="2">2</option>
-                                                                            <option value="3">3</option>
-                                                                            <option value="4">4</option>
-                                                                            <option value="5">5</option>
-                                                                        </select>
-                                                                    </span>
-                                                                </p>
-                                                                <p class="feedback-form">
-                                                                    <label for="feedback">Your Review</label>
-                                                                    <textarea id="feedback" name="comment" cols="45" rows="8" aria-required="true"></textarea>
-                                                                </p>
-                                                                <div class="feedback-input">
-                                                                    <p class="feedback-form-author">
-                                                                        <label for="author">Name<span class="required">*</span>
-                                                                        </label>
-                                                                        <input id="author" name="author" value="" size="30" aria-required="true" type="text">
-                                                                    </p>
-                                                                    <p class="feedback-form-author feedback-form-email">
-                                                                        <label for="email">Email<span class="required">*</span>
-                                                                        </label>
-                                                                        <input id="email" name="email" value="" size="30" aria-required="true" type="text">
-                                                                        <span class="required"><sub>*</sub> Required fields</span>
-                                                                    </p>
-                                                                    <div class="feedback-btn pb-15">
-                                                                        <a href="#" class="close" data-dismiss="modal" aria-label="Close">Close</a>
-                                                                        <a href="#">Submit</a>
-                                                                    </div>
-                                                                </div>
-                                                            </form>
-                                                        </div>
-                                                    </div>
-                                                    <!-- Feedback Area End Here -->
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- Quick View | Modal Area End Here -->
-                    </div>
-                </div>
             </div>
         </div>
     </div>
